19bb42a9a98d35dc0e6c3f81b31847edfa6ef409
Jeffrey Chang <tsavij@gmail.com>
2018-10-11 02:09:39 -0400

    setting up board touch

--- app/src/main/java/com/jhchang/simplesudoku/GameView.java
+++ app/src/main/java/com/jhchang/simplesudoku/GameView.java
@@ -39,6 +39,7 @@ public class GameView extends SurfaceView implements Runnable {
     private int[] bsTimer = new int [9];
     private int[] targetX = new int[9];
     private int[] targetY = new int[9];
+    private int[] activeTouch = {-1,-1};
     //context to be used in onTouchEvent to cause the activity transition from GameAvtivity to MainActivity.
     private Context context;
     //board dimensions
@@ -47,6 +48,7 @@ public class GameView extends SurfaceView implements Runnable {
     private int[] boardFour=new int[4];
     //board
     //ArrayList<int[]> currBoard=new ArrayList<int[]>();
+    private int board;
     private Cell[][] currBoard;
     ArrayList<int[]> candList=new ArrayList<int[]>();
     int[] squareSelect = new int[2];
@@ -125,163 +127,13 @@ public class GameView extends SurfaceView implements Runnable {
     private Cell[][] generateBoard(String msg, int diff){
         int[] square = new int[4]; //input value 0-8, fixed num value 0-1, select value 0-1, highlight value 0-1
         int boardSize = msg.length();
+        board = boardSize;
         //int boardSize = 4;
 
 
         sudokuGen = new Sudoku(boardSize, diff);
 
         return sudokuGen.getBoard();
-        /* using squares
-        //currBoard.clear();
-        for(int y = 0;y<boardSize;y++){
-            for(int x = 0;x<boardSize;x++){
-                square[0] = tempBoard[x][y].getNum();
-                if(tempBoard[x][y].getNum() > 0){
-                    square[1] = 1;
-                }else{
-                    square[1] = 0;
-                }
-                square[2] = 0;
-                square[3] = 0;
-                currBoard.add(square);
-                System.out.print(tempBoard[x][y].getNum()+"  ");
-            }
-            System.out.println();
-            System.out.println();
-        }
-
-                */
-        /*
-        int[][] tempMat = sudokuGen.getMat();
-
-        currBoard.clear();
-        for(int y = 0;y<boardSize;y++){
-            for(int x = 0;x<boardSize;x++){
-                square[0] = tempMat[x][y];
-                square[1] = 0; //temp
-                square[2] = 0; //temp
-                square[3] = 0; //temp
-                currBoard.add(square);
-            }
-        }
-        sudokuGen.printSudoku();
-        */
-        /*
-
-        CURRENT PROBLEM WITH THIS SHIT IS THAT I ACTUALLY NEED TO SOLVE THE SUDOKU BOARD IN ORDER TO GENERATE IT, SO THIS IS PRETTY MUCH ALL TRASH??
-
-        
-
-         */
-
-
-
-
-        /* You can use this to check if board complete or board errors, but for now, it's no good for generating
-        //Start with creating a fully completed board first
-        ArrayList<Integer> tempBoard=new ArrayList<Integer>();
-        int boardSize = msg.length();
-
-        //board break points
-        ArrayList<int[]> breakNine = new ArrayList<int[]>(); //break points for 9x9, though this is ugly, I think this faster than doing it with a loop
-        int[] nb = {3,3}; breakNine.add(nb); nb = new int[] {6,3}; breakNine.add(nb); nb = new int[] {9,3}; breakNine.add(nb);
-        nb = new int[] {3,6}; breakNine.add(nb); nb = new int[] {6,6}; breakNine.add(nb); nb = new int[] {9,6}; breakNine.add(nb);
-        nb = new int[] {3,9}; breakNine.add(nb); nb = new int[] {6,9}; breakNine.add(nb); nb = new int[] {9,9}; breakNine.add(nb);
-        int bWidth = 3;
-        int bHeight = 3;
-
-        ArrayList<int[]> breakSix = new ArrayList<int[]>(); //break points for 6x6
-        int[] sb = {3,2}; breakSix.add(sb); sb = new int[] {6,2}; breakSix.add(sb);
-        sb = new int[] {3,4}; breakSix.add(sb);sb = new int[] {6,4}; breakSix.add(sb);
-        sb = new int[] {3,6}; breakSix.add(sb);sb = new int[] {6,6}; breakSix.add(sb);
-
-        ArrayList<int[]> breakFour = new ArrayList<int[]>(); // break points for 4x4
-        int[] fb = {2,2}; breakFour.add(fb); fb = new int[] {4,2}; breakFour.add(fb);
-        fb = new int[] {2,4}; breakFour.add(fb); fb = new int[] {4,4}; breakFour.add(fb);
-
-        for(int i = 0;i<boardSize*boardSize;i++){
-            int[] cList = new int [boardSize];
-            for(int c = 0;c<boardSize;c++){
-                cList[c] = c;
-            }
-            candList.add(cList);
-        }
-        for(int x = 0; x<boardSize;x++){
-            for(int y = 0;y < boardSize;y++){
-                int[] cList = candList.get(x+(boardSize*y));
-
-                System.out.println("x: "+ x + " y: " + y + " cList: " + cList.length);
-                int getPos = 0;
-                if(cList.length > 1){
-                    getPos = (int)(Math.random()*cList.length);
-                }
-                int val = cList[getPos];
-                System.out.println("val: " + val);
-                tempBoard.add(val); //randomly add value from candidate list
-                //removeElement(cList,getPos);
-                cList = ArrayUtils.remove(cList,getPos);
-                candList.set(x+(boardSize*y),cList);
-                //remove value from candidate lists in same row, column and box
-                for(int xx = 0;xx<boardSize;xx++){ // remove cand from row
-                    if(xx != x){
-                        int[] tcList = candList.get(xx+(boardSize*y));
-                        int tcPos = ArrayUtils.indexOf(tcList,val);
-                        if(tcPos > -1){
-                            tcList = ArrayUtils.remove(tcList,tcPos);
-                            candList.set(xx+(boardSize*y),tcList);
-                        }
-                    }
-                }
-                for(int yy = 0;yy<boardSize;yy++){ //remove cand from col
-                    if(yy != y){
-                        int[] tcList = candList.get(x+(boardSize*yy));
-                        int tcPos = ArrayUtils.indexOf(tcList,val);
-                        if(tcPos > -1){
-                            tcList = ArrayUtils.remove(tcList,tcPos);
-                            candList.set(x+(boardSize*yy),tcList);
-                        }
-                    }
-
-                }
-                //remove cand from box
-                ArrayList<int[]> breakPoints;
-                int[] bPoint = new int[2];
-                if(boardSize == 9){
-                    breakPoints = breakNine;
-                }else if(boardSize == 6){
-                    breakPoints = breakSix;
-                }else if(boardSize == 4){
-                    breakPoints = breakFour;
-                }else{
-                    breakPoints = breakNine;
-                }
-                for(int b = 0;b<breakPoints.size();b++){
-                    if(x< breakPoints.get(b)[0] & y< breakPoints.get(b)[1]){
-                        bPoint = breakPoints.get(b);
-                        //System.out.println("break, x: "+bPoint[0]+", y: "+bPoint[1]);
-                        break;
-                    }
-                }
-                for(int xx = bPoint[0]-bWidth;xx<bPoint[0];xx++){
-                    for(int yy = bPoint[1]-bHeight;yy<bPoint[1];yy++){
-                        if(xx != x & yy != y){
-                            int[] tcList = candList.get(xx+(boardSize*yy));
-                            int tcPos = ArrayUtils.indexOf(tcList,val);
-                            if(tcPos > -1){
-                                tcList = ArrayUtils.remove(tcList,tcPos);
-                                candList.set(xx+(boardSize*yy),tcList);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        //end of full board generate
-        for(int i=0;i<boardSize*boardSize;i++){
-            int[] tempL = {tempBoard.get(i),0,0,0};
-            currBoard.add(tempL);
-        }
-        */
 
     }
 
@@ -310,6 +162,7 @@ public class GameView extends SurfaceView implements Runnable {
                 case 5:
                     //draw sudoku board
                     drawBoard(canvas, "DISMANTLE",Color.WHITE,Color.BLACK);
+                    drawAvailNum(canvas, "DISMANTLE",Color.WHITE);
                     //drawBoard(canvas, "WAKE",Color.WHITE,Color.BLACK);
                     break;
                 case 4:
@@ -327,7 +180,7 @@ public class GameView extends SurfaceView implements Runnable {
                     //3 to -1 is scene text
 
 
-                    dProg = 5; //EDIT THIS OUT LATER
+                    //dProg = 5; //EDIT THIS OUT LATER
                     dprogFlag = false;
                 case 2:
                     dialogue(canvas,"WAKE", posMid, 2,"none", normText, bigFont);
@@ -354,12 +207,15 @@ public class GameView extends SurfaceView implements Runnable {
         }
     }
 
+
     private void drawBoard(Canvas c, String msg, int priColor, int secColor){
         int boardSize = msg.length();
         paint.setColor(priColor);
         int[] bCoords;
-        int xSpace = 0, ySpace = 0;
         switch(boardSize){
+            case 9:
+                bCoords = boardNine;
+                break;
             case 6:
                 bCoords = boardSix;
                 break;
@@ -368,12 +224,38 @@ public class GameView extends SurfaceView implements Runnable {
                 break;
             default:
                 bCoords = boardNine;
+                break;
         }
         //draw primary background
         c.drawRect(bCoords[0],bCoords[1],bCoords[2],bCoords[3],paint);
         //draw individual blocks
         paint.setColor(secColor);
-        for(int x = 0;x< boardSize;x++){
+
+        drawNineBoard(c,msg,priColor,secColor,bCoords);
+
+        /* draw for specific board, implement once you have all board info set up
+        switch(boardSize){
+            case 9:
+                drawNineBoard(c,msg,priColor,secColor,bCoords);
+                break;
+            case 6:
+                drawSixBoard(c,msg,priColor,secColor,bCoords);
+                break;
+            case 4:
+                drawFourBoard(c,msg,priColor,secColor,bCoords);
+                break;
+            default:
+                drawNineBoard(c,msg,priColor,secColor,bCoords);
+                break;
+        }
+        */
+
+
+
+    }
+    private void drawNineBoard(Canvas c, String msg, int priColor, int secColor, int[] bCoords){
+        int xSpace = 0, ySpace = 0;
+        for(int x = 0;x< board;x++){
             switch(x){
                 case 0:
                     xSpace = 12;
@@ -390,7 +272,7 @@ public class GameView extends SurfaceView implements Runnable {
                     xSpace+=2;
                     break;
             }
-            for(int y = 0;y<boardSize;y++){
+            for(int y = 0;y<board;y++){
                 switch(y){
                     case 0:
                         ySpace = 12;
@@ -407,14 +289,12 @@ public class GameView extends SurfaceView implements Runnable {
                         ySpace+=2;
                         break;
                 }
-
                 paint.setColor(secColor);
                 int tempX = bCoords[0]+xSpace+boxSize*x;
                 int tempY = bCoords[1]+ySpace+boxSize*y;
                 c.drawRect(tempX,tempY,tempX+boxSize,tempY+boxSize,paint);
 
                 // draw nums from board, still testing
-
                 paint.setColor(priColor);
                 paint.setTextSize(medFont);
                 int squareVal = currBoard[x][y].getNum();
@@ -422,19 +302,101 @@ public class GameView extends SurfaceView implements Runnable {
                     String tempMsg =  String.valueOf(msg.charAt(squareVal - 1));
                     c.drawText(tempMsg,tempX,tempY+boxSize,paint);
                 }
-                /* using squares and arraylist
-                int[] currSquare = currBoard.get(x+(y*boardSize));
-                int pos = currSquare[0];
-                c.drawText(msg.substring(pos,1), tempX, tempY+boxSize,paint);
-                */
-
-                /* basic grid with no variation
-                int xCoord = startX+5+5*x+x*boxSize;
-                int yCoord = startY+5+5*y+y*boxSize;
-                c.drawRect(xCoord,yCoord,xCoord+boxSize,yCoord+boxSize,paint);
-                */
             }
         }
+    }
+
+    private void drawSixBoard(Canvas c, String msg, int priColor, int secColor, int[] bCoords){
+        int xSpace = 0, ySpace = 0;
+        for(int x = 0;x< board;x++){
+            switch(x){
+                case 0:
+                    xSpace = 12;
+                    break;
+                case 3:
+                    xSpace+=6;
+                case 1:
+                case 2:
+                case 4:
+                case 5:
+                    xSpace+=2;
+                    break;
+            }
+            for(int y = 0;y<board;y++){
+                switch(y){
+                    case 0:
+                        ySpace = 12;
+                        break;
+                    case 2:
+                    case 4:
+                        ySpace+=6;
+                    case 1:
+                    case 3:
+                    case 5:
+                        ySpace+=2;
+                        break;
+                }
+                paint.setColor(secColor);
+                int tempX = bCoords[0]+xSpace+boxSize*x;
+                int tempY = bCoords[1]+ySpace+boxSize*y;
+                c.drawRect(tempX,tempY,tempX+boxSize,tempY+boxSize,paint);
+
+                // draw nums from board, still testing
+                paint.setColor(priColor);
+                paint.setTextSize(medFont);
+                int squareVal = currBoard[x][y].getNum();
+                if(squareVal > 0){
+                    String tempMsg =  String.valueOf(msg.charAt(squareVal - 1));
+                    c.drawText(tempMsg,tempX,tempY+boxSize,paint);
+                }
+            }
+        }
+    }
+
+    private void drawFourBoard(Canvas c, String msg, int priColor, int secColor, int[] bCoords){
+        int xSpace = 0, ySpace = 0;
+        for(int x = 0;x< board;x++){
+            switch(x){
+                case 0:
+                    xSpace = 12;
+                    break;
+                case 2:
+                    xSpace+=6;
+                case 1:
+                case 3:
+                    xSpace+=2;
+                    break;
+            }
+            for(int y = 0;y<board;y++){
+                switch(y){
+                    case 0:
+                        ySpace = 12;
+                        break;
+                    case 2:
+                        ySpace+=6;
+                    case 1:
+                    case 3:
+                        ySpace+=2;
+                        break;
+                }
+                paint.setColor(secColor);
+                int tempX = bCoords[0]+xSpace+boxSize*x;
+                int tempY = bCoords[1]+ySpace+boxSize*y;
+                c.drawRect(tempX,tempY,tempX+boxSize,tempY+boxSize,paint);
+
+                // draw nums from board, still testing
+                paint.setColor(priColor);
+                paint.setTextSize(medFont);
+                int squareVal = currBoard[x][y].getNum();
+                if(squareVal > 0){
+                    String tempMsg =  String.valueOf(msg.charAt(squareVal - 1));
+                    c.drawText(tempMsg,tempX,tempY+boxSize,paint);
+                }
+            }
+        }
+    }
+
+    private void drawAvailNum(Canvas c, String msg, int priColor) {
 
     }
 
@@ -544,19 +506,107 @@ public class GameView extends SurfaceView implements Runnable {
         }
     }
 
+    private void pointerLoc(int touchX, int touchY){
+        int[] bCoords;
+        int xSpace = 0, ySpace = 0;
+        switch(board){
+            case 9:
+                bCoords = boardNine;
+                break;
+            case 6:
+                bCoords = boardSix;
+                break;
+            case 4:
+                bCoords = boardFour;
+                break;
+            default:
+                bCoords = boardNine;
+                break;
+        }
+
+        /* will have to set up board specific locator func since their spacing is different
+        switch(board){
+            case 9:
+                locNineBoard(touchX,touchY,bCoords);
+                break;
+        }
+        */
+        xloop:
+        for(int x = 0;x< board;x++){
+            switch(x){
+                case 0:
+                    xSpace = 12;
+                    break;
+                case 3:
+                case 6:
+                    xSpace+=6;
+                case 1:
+                case 2:
+                case 4:
+                case 5:
+                case 7:
+                case 8:
+                    xSpace+=2;
+                    break;
+            }
+            for(int y = 0;y<board;y++){
+                switch(y){
+                    case 0:
+                        ySpace = 12;
+                        break;
+                    case 3:
+                    case 6:
+                        ySpace+=6;
+                    case 1:
+                    case 2:
+                    case 4:
+                    case 5:
+                    case 7:
+                    case 8:
+                        ySpace+=2;
+                        break;
+                }
+
+                int tempX = bCoords[0]+xSpace+boxSize*x;
+                int tempY = bCoords[1]+ySpace+boxSize*y;
+                //extrapolate position and dimension based off of below
+                //c.drawRect(tempX,tempY,tempX+boxSize,tempY+boxSize,paint);
+
+                if(touchX > tempX & touchY > tempY & touchX < tempX+boxSize & touchY < tempY+boxSize){
+                    activeTouch[0]=x;
+                    activeTouch[1]=y;
+                    System.out.println("tap at touchX: " + touchX + " touchY: " + touchY);
+                    System.out.println("touch identified at location x: " + x + " y: " + y);
+                    break xloop;
+                }
+            }
+        }
+    }
+
     @Override
     public boolean onTouchEvent(MotionEvent motionEvent) {
         int touchX = (int)motionEvent.getX();
         int touchY = (int)motionEvent.getY();
-        switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
-            case MotionEvent.ACTION_DOWN:
-                if(touchY > screenY/2 + bigFont & touchY < screenY/2 - bigFont){
-                    dProg = 3; //edit this make sure it's good
-                    dprogFlag = false;
-                }
-                break;
-            case MotionEvent.ACTION_UP:
-                break;
+        if(dProg < 3){
+            switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
+                case MotionEvent.ACTION_DOWN:
+                    if(touchY > screenY/2 + bigFont & touchY < screenY/2 - bigFont){
+                        dProg = 5; //edit this make sure it's good
+                        dprogFlag = false;
+                    }
+                    break;
+                case MotionEvent.ACTION_UP:
+                    break;
+            }
+        }else if(dProg == 5){
+            int index = motionEvent.getActionIndex();
+            int action = motionEvent.getActionMasked();
+            int pointerId = motionEvent.getPointerId(index);
+            switch(action){
+                case MotionEvent.ACTION_DOWN:
+                    pointerLoc(touchX,touchY);
+                    break;
+            }
         }
         return true;
     }
